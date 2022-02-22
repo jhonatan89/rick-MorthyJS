a7c2d5519404f09507fecfb0ae62f009e623e16d
Nicolas Orjuela <n.orjuela@uniandes.edu.co>
2022-02-22 17:15:57 -0500

    bono dom

--- js/script.js
+++ js/script.js
@@ -12,9 +12,31 @@ async function getData() {
 async function render(){
     const data = await getData();
     const dataMapped = data.results.map((character) => {
-        const p = document.createElement('p');
-        p.textContent = character.name;
-        containerRef.appendChild(p)
+        
+        let card = document.createElement('div');
+        card.className = 'card';
+      
+        let cardImg = document.createElement('img');
+        cardImg.className = 'card-img-top';
+        cardImg.src = character.image;
+
+        let cardBody = document.createElement('div');
+        cardBody.className = 'card-body';
+      
+        let title = document.createElement('h5');
+        title.innerText = character.name;
+        title.className = 'card-title';
+      
+        let text = document.createElement('p');
+        text.innerText = character.status;
+        text.className = 'card-text';
+      
+      
+        card.appendChild(cardImg);
+        cardBody.appendChild(title);
+        cardBody.appendChild(text);
+        card.appendChild(cardBody);
+        containerRef.appendChild(card);
     })
 }
 

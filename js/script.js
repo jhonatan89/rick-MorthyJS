d735073f411be53a4e8d0f296320683d591ef843
gbeltranl <69653044+gbeltranl@users.noreply.github.com>
2022-02-24 18:05:33 -0500

    finish3

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const API_RICK_MORTHY = 'https://rickandmortyapi.com/api/character';
 const containerRef = document.getElementById('container');
-document.que
+
 
 async function getData() {
   const result = await fetch(API_RICK_MORTHY);
@@ -8,14 +8,7 @@ async function getData() {
   console.log('result', data);
   return data;
 }
-/* "<div class="card" style="width: 18rem;">
-        <img class="card-img-top" src="..." alt="Card image cap">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-      </div>" */
+
 async function render(){
     const data = await getData();
     const row = document.createElement('div');

3fe59137f901c9ab2e598958e504666ad2e4557e
gbeltranl <g.beltranl@uniandes.edu.co>
2022-02-27 23:48:46 -0500

    Added Detail

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const API_RICK_MORTHY = 'https://rickandmortyapi.com/api/character';
 const containerRef = document.getElementById('container');
-document.que
+const detailRef = document.getElementById('detail');
 
 async function getData() {
   const result = await fetch(API_RICK_MORTHY);
@@ -8,15 +8,16 @@ async function getData() {
   console.log('result', data);
   return data;
 }
-/* "<div class="card" style="width: 18rem;">
-        <img class="card-img-top" src="..." alt="Card image cap">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-      </div>" */
+
+async function getEpisodeData(url) {
+  const result = await fetch(url);
+  const episode = await result.json();
+  console.log('result', episode);
+  return episode.name;
+}
+
 async function render(){
+    detailRef.style.display= "none";
     const data = await getData();
     const row = document.createElement('div');
     row.className = 'row gy-5 mx-n5';
@@ -34,7 +35,7 @@ async function render(){
 
         const body = document.createElement('div');
         body.className = 'card-body'
-          const title = document.createElement('h5');
+          const title = document.createElement('h4');
           title.innerText = character.name;
           title.className = 'card-title';
           body.appendChild(title);
@@ -78,6 +79,16 @@ async function render(){
             origin_row.appendChild(origin);
           body.appendChild(origin_row);
 
+          const button_detail = document.createElement('div');
+          
+          button_detail.className = 'btn btn-primary';
+          button_detail.onclick = function(){
+           showDetail(character);
+          };
+          button_detail.innerText = "Show detail";
+          body.appendChild(button_detail);
+          
+
         card.appendChild(img);
         card.appendChild(body);
 
@@ -86,6 +97,109 @@ async function render(){
       containerRef.appendChild(row);
        
     })
+    
+    
 }
+
+async function renderDetail(character) {
+  
+  console.log(character.name);
+  
+  const row = document.createElement('div');
+  row.className = 'row ';
+    const left = document.createElement('div');
+    left.className = 'col-4 p-0';
+    left.style.backgroundColor = "#A8A8A8";
+    const char_img = document.createElement('img');
+    char_img.className = 'rounded col-12 img-fluid m-0';
+    char_img.src = character.image;
+    char_img.alt = character.name;
+    left.appendChild(char_img);
+    const right = document.createElement('div');
+    right.className= 'col-8 p-0';
+    right.style.backgroundColor = "#EBECF0";
+    row.appendChild(left);
+    row.appendChild(right);
+
+      const right_row = document.createElement('div');
+      right_row.className = 'row m-0';
+
+        const right_btn = document.createElement('button');
+        right_btn.className = 'btn btn-close offset-11 col-1';
+        right_btn.onclick = function () { detailRef.removeChild(row); showList();}
+        right_row.appendChild(right_btn);
+        
+        const name = document.createElement('h2');
+        name.innerText = character.name;
+        right_row.appendChild(name);
+
+        const gender_lbl = document.createElement('h4');
+        gender_lbl.className = 'col-6';
+        gender_lbl.innerText = 'Gender';
+        right_row.appendChild(gender_lbl);
+
+        const gender = document.createElement('h4');
+        gender.className = 'col-6';
+        gender.innerText = character.gender;
+        right_row.appendChild(gender);
+
+        const species_lbl = document.createElement('h4');
+        species_lbl.className = 'col-6';
+        species_lbl.innerText = 'Species';
+        right_row.appendChild(species_lbl);
+
+        const species = document.createElement('h4');
+        species.className = 'col-6';
+        species.innerText = character.species;
+        right_row.appendChild(species);
+
+        const location_lbl = document.createElement('h4');
+        location_lbl.className = 'col-6';
+        location_lbl.innerText = 'Location';
+        right_row.appendChild(location_lbl);
+
+        const location = document.createElement('h4');
+        location.className = 'col-6';
+        location.innerText = character.location.name;
+        right_row.appendChild(location);
+
+        const episodes_lbl = document.createElement('h4');
+        episodes_lbl.className = 'col-6';
+        episodes_lbl.innerText = 'Episodes';
+        right_row.appendChild(episodes_lbl);
+
+        const episodes = document.createElement('h4');
+        episodes.className = 'col-6';
+        for(const episode of character.episode){
+          const episode_name = await getEpisodeData(episode);
+          console.log(episode_name);
+          episodes.append(episode_name);
+          episodes.append(", ");
+        }
+        
+        right_row.appendChild(episodes);
+
+        
+        
+        right.appendChild(right_row);
+        
+
+  detailRef.appendChild(row);
+  console.log(detailRef);
+
+} 
+
+async function showDetail(character){
+  renderDetail(character);
+  containerRef.style.display= 'none';
+  detailRef.style.display = 'block';
+
+}
+async function showList(){
+  containerRef.style.display= 'block';
+  detailRef.style.display = 'none';
+
+}
+
 
 render();
